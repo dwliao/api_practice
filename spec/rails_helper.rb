89a30969ad511335fae640b8278a4c1dd774b71c
dwliao <dw.liao@hotmail.com>
2017-05-06 22:51:17 +0800

    Adds sessions controller create action

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,7 +35,7 @@ RSpec.configure do |config|
 
   #Including to test requests
   config.include Request::JsonHelpers, :type => :controller
-  
+
   config.include Request::HeadersHelpers, :type => :controller
 
   config.before(:each, type: :controller) do
